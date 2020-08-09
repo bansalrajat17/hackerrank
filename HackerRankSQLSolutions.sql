1cb05312a2076b198d174b2f78e89aa9c7444c85
bansalrajat17 <bansalrajat17@github.com>
2020-08-09 08:59:31 +0530

    SQL PROBLEMS AND ANSWERS UPTO PROBLEM 8

---
+++ HackerRankSQLSolutions.sql
@@ -0,0 +1,35 @@
+/* Solution - 1 */
+
+SELECT name FROM Employee order by name asc;
+
+/* Solution - 2 */
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
+
+/* Solution - 3 */
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
+
+/* Solution - 4 */
+
+SELECT * FROM CITY;
+
+/* Solution - 5 */
+
+SELECT * FROM CITY WHERE ID = 1661;
+
+/* Solution - 6 */
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN'; 
+
+
+/* Solution - 7 */
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE='JPN';
+
+
+/* Solution - 8 */
+
+SELECT CITY,STATE FROM STATION;
+
+
